962cd51462a05432edec4e6603b2472c7b335b71
vegcom <189027736+vegcom@users.noreply.github.com>
2026-01-22 01:25:36 -0600

    a piece of me

---
+++ config.d/aliases.ps1
@@ -0,0 +1,6 @@
+Set-Alias -Name ep -Value Edit-PowerShellProfile
+Set-Alias -Name pnl -Value Playnite-Extention-Logs
+Set-Alias -Name pnl -Value Playnite-Logs
+Set-Alias -Name vc-logs -Value Get-VSCodeLatestExthostLog
+Set-Alias -Name chkvars -Value Check-Vars
+Set-Alias -Name k -Value kubecolor
